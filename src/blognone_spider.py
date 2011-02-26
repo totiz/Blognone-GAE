89102a8a61806623a225a13aa61f17c2b3727c6e
Nattapon Nimakul <totizy@gmail.com>
2011-02-27 01:29:13 +0700

    separate content and author of comment into diction

--- src/blognone_spider.py
+++ src/blognone_spider.py
@@ -52,6 +52,7 @@ class Comment_Spider(HTMLParser):
         self.Comments = []
         self.comment_id = -1
         self.comment_data = []
+        self.comment_author = ''
         self.div_stage = ''
         #self.feed(req.content)
         self.feed(unicode(req.content, 'utf-8'))
@@ -134,8 +135,15 @@ class Comment_Spider(HTMLParser):
                 #self.Comments.append(self.comment_id)
                 #self.comment_data = self.comment_data + ' <span style="color:#999999;">' + self.comment_id + '</span>'
                 self.comment_data.append(self.comment_id)
-                self.Comments.append(self.comment_data)
+                
+                comment = {
+                    'content': self.comment_data,
+                    'author': self.comment_author
+                }
+                
+                self.Comments.append(comment)
                 self.comment_data = []
+                self.comment_author = ''
                 self.comment_id = -1
                 self.div_stage = ''
             
@@ -147,5 +155,6 @@ class Comment_Spider(HTMLParser):
                 
         if self.div_stage == 'comment-info' and data.find('By:') <> -1:
             #self.comment_data = data + '\n<br>'
-            self.comment_data.append(data)
+            self.comment_author = data
+            #self.comment_data.append(data)
 
